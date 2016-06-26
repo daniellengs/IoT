71def7ad06a598655f67841e765b5fd262afed8f
Diego Souza <dts@cesar.org.br>
2016-06-26 15:56:51 -0300

    intent filter samples

---
+++ IntentFilter/app/src/main/java/br/com/dts/intentfilter/FilterActivity.java
@@ -0,0 +1,12 @@
+package br.com.dts.intentfilter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ IntentFilter/app/src/main/java/br/com/dts/intentfilter/ProtectedActivity.java
@@ -0,0 +1,13 @@
+package br.com.dts.intentfilter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProtectedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_protected);
+    }
+}
