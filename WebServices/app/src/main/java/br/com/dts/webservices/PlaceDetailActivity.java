ed6e6b1c67f1e498f52e9af8ef3e672a28470b2f
Diego Souza <dts@cesar.org.br>
2016-08-02 13:35:05 -0300

    Broadcast project

--- WebServices/app/src/main/java/br/com/dts/webservices/PlaceDetailActivity.java
+++ WebServices/app/src/main/java/br/com/dts/webservices/PlaceDetailActivity.java
@@ -6,6 +6,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.LatLng;
+
 import br.com.dts.webservices.model.Place;
 
 public class PlaceDetailActivity extends AppCompatActivity {
@@ -13,6 +19,9 @@ public class PlaceDetailActivity extends AppCompatActivity {
     private TextView mTextName;
     private TextView mTextDetail;
     private Button mBtnRoute;
+    private MapView mMapView;
+
+    private GoogleMap mMap;
 
     private Place mPlace;
 
@@ -21,8 +30,12 @@ public class PlaceDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_detail);
 
+        mPlace = (Place) getIntent().getSerializableExtra(ListPlacesActivity.EXTRA_PLACE);
+
         mTextName = (TextView) findViewById(R.id.txt_place_name);
         mTextDetail = (TextView) findViewById(R.id.txt_place_details);
+
+        setupMap();
 
         mBtnRoute = (Button) findViewById(R.id.btn_route);
 
@@ -33,11 +46,24 @@ public class PlaceDetailActivity extends AppCompatActivity {
             }
         });
 
-        mPlace = (Place) getIntent().getSerializableExtra(ListPlacesActivity.EXTRA_PLACE);
-
         if (mPlace != null){
             mTextName.setText(mPlace.getProperties().getPTurist());
             mTextDetail.setText(mPlace.getProperties().getDescritv());
         }
+    }
+
+    private void setupMap(){
+        mMapView = (MapView) findViewById(R.id.mapview);
+
+        mMap = mMapView.getMap();
+
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        mMap.setMyLocationEnabled(true);
+
+        double latitude = Double.parseDouble(mPlace.getGeometry().getCoordinates()[0]);
+        double longitude = Double.parseDouble(mPlace.getGeometry().getCoordinates()[1]);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 10);
+        mMap.animateCamera(cameraUpdate);
     }
 }
